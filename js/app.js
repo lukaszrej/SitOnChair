4f78c84236994d86a96fb5ee65f8b8f67d988012
Lukasz Rej <rejlukasz@gmail.com>
2019-04-11 22:48:18 +0200

    Slider - basic functionality

--- js/app.js
+++ js/app.js
@@ -3,30 +3,36 @@ document.addEventListener('DOMContentLoaded', function () {
     // slider //
     const buttonPrev = document.querySelector('.slider__nav--prev');
     const buttonNext = document.querySelector('.slider__nav--next');
-    const sliderCol = document.getElementsByClassName('slider__col');
+    const slides = document.querySelectorAll('.container .slides');
 
-    sliderCol[2].style.display = "none";
-    sliderCol[3].style.display = "none";
-
-    let currentSlide = 0;
+    let activeSlide = 0;
+    slides[activeSlide].classList.add('visible');
 
     buttonPrev.addEventListener('click', () => {
-        sliderCol[2].style.display = "none";
-        sliderCol[3].style.display = "none";
-        sliderCol[0].style.display = "block";
-        sliderCol[1].style.display = "block";
-        sliderCol[0].classList.add('focus-in');
-        sliderCol[1].classList.add('focus-in');
+        slides[activeSlide].classList.remove('visible');
+
+        activeSlide--;
+
+        if (activeSlide < 0) {
+            activeSlide = slides.length - 1;
+        }
+
+        slides[activeSlide].classList.add('visible');
     });
 
     buttonNext.addEventListener('click', () => {
-        sliderCol[0].style.display = "none";
-        sliderCol[1].style.display = "none";
-        sliderCol[2].classList.add('focus-in');
-        sliderCol[3].classList.add('focus-in');
-        sliderCol[2].style.display = "block";
-        sliderCol[3].style.display = "block";
+        slides[activeSlide].classList.remove('visible');
+
+        activeSlide++;
+
+        if (activeSlide >= slides.length) {
+
+            activeSlide = 0;
+        }
+
+        slides[activeSlide].classList.add('visible');
     });
+
 
 
 });
